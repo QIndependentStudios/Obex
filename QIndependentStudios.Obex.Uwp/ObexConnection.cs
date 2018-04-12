f070b09ded269ee54aa4289fbc588f66e34b6da9
Quoc-Thy Ngo <qngo.general@outlook.com>
2018-04-12 15:06:17 -0400

    Added documentation to UWP connection classes.

--- QIndependentStudios.Obex.Uwp/ObexConnection.cs
+++ QIndependentStudios.Obex.Uwp/ObexConnection.cs
@@ -8,21 +8,47 @@ using Windows.Storage.Streams;
 
 namespace QIndependentStudios.Obex.Uwp
 {
+    /// <summary>
+    /// Provides implementation of an Obex connection.
+    /// </summary>
     public abstract class ObexConnection : IObexConnection
     {
+        /// <summary>
+        /// The underlying <see cref="StreamSocket"/> connection.
+        /// </summary>
         protected StreamSocket _socket;
 
+        /// <summary>
+        /// Gets the underlying <see cref="StreamSocket"/> connection.
+        /// </summary>
         public StreamSocket Socket => _socket;
 
+        /// <summary>
+        /// Creates an new instance of an <see cref="ObexConnection"/> to an <see cref="RfcommDeviceService"/>.
+        /// </summary>
+        /// <param name="rfcommService">The rfcomm service to connect to.</param>
+        /// <returns>Returns an Obex connection for the specified rfcomm service.</returns>
         public static ObexConnection Create(RfcommDeviceService rfcommService)
         {
             return new RfcommServiceObexConnection(rfcommService);
         }
 
+        /// <summary>
+        /// Creates and initializes a connection if not already done so. This is called before any read and write.
+        /// </summary>
+        /// <returns></returns>
         public abstract Task EnsureInitAsync();
 
+        /// <summary>
+        /// Frees up and releases resources.
+        /// </summary>
         public abstract void Dispose();
 
+        /// <summary>
+        /// Provides a way for the Obex client to receive data over the connection.
+        /// </summary>
+        /// <param name="length">The number of bytes to read.</param>
+        /// <returns>The bytes read from the connection.</returns>
         public async Task<byte[]> ReadAsync(uint length)
         {
             var bytes = new byte[length];
@@ -30,6 +56,11 @@ namespace QIndependentStudios.Obex.Uwp
             return bytes;
         }
 
+        /// <summary>
+        /// Provides a way for the Obex client to send data over the connection.
+        /// </summary>
+        /// <param name="data">The data the client wants to send.</param>
+        /// <returns></returns>
         public async Task WriteAsync(byte[] data)
         {
             await _socket.OutputStream.WriteAsync(data.AsBuffer());
--- QIndependentStudios.Obex.Uwp/ObexServerConnection.cs
+++ QIndependentStudios.Obex.Uwp/ObexServerConnection.cs
@@ -9,27 +9,59 @@ using Windows.Storage.Streams;
 
 namespace QIndependentStudios.Obex.Uwp
 {
+    /// <summary>
+    /// Provides implementation of an Obex server connection.
+    /// </summary>
     public abstract class ObexServerConnection : IObexServerConnection
     {
+        /// <summary>
+        /// The underlying <see cref="StreamSocket"/> connection.
+        /// </summary>
         protected StreamSocket _socket;
 
+        /// <summary>
+        /// Gets the underlying <see cref="StreamSocket"/> connection.
+        /// </summary>
         public StreamSocket Socket => _socket;
 
+        /// <summary>
+        /// Creates an new instance of an <see cref="ObexServerConnection"/> for a discoverable <see cref="RfcommServiceId"/>.
+        /// </summary>
+        /// <param name="serviceId">The service that will be advertised for a connection over rfcomm.</param>
+        /// <returns>Returns an Obex server connection for the specified rfcomm service id.</returns>
         public static ObexServerConnection Create(RfcommServiceId serviceId)
         {
             return new RfcommServiceObexServerConnection(serviceId, null);
         }
 
+        /// <summary>
+        /// Creates an new instance of an <see cref="ObexServerConnection"/> for a discoverable <see cref="RfcommServiceId"/>.
+        /// </summary>
+        /// <param name="serviceId">The service that will be advertised for a connection over rfcomm.</param>
+        /// <param name="attributes">The rfcomm attributes that can be queried by potential clients.</param>
+        /// <returns>Returns an Obex server connection for the specified rfcomm service id.</returns>
         public static ObexServerConnection Create(RfcommServiceId serviceId,
             IDictionary<byte, byte[]> attributes)
         {
             return new RfcommServiceObexServerConnection(serviceId, attributes);
         }
 
+        /// <summary>
+        /// Creates a connection and starts listening for a client to connect over that connection.
+        /// </summary>
+        /// <returns></returns>
         public abstract Task WaitForClientAsync();
 
+        /// <summary>
+        /// Frees up and releases resources.
+        /// </summary>
         public abstract void Dispose();
 
+        /// <summary>
+        /// Provides a way for the Obex server to receive data over the connection.
+        /// </summary>
+        /// <param name="length"></param>
+        /// <returns>The bytes read from the connection.</returns>
         public async Task<byte[]> ReadAsync(uint length)
         {
             var bytes = new byte[length];
@@ -37,6 +69,11 @@ namespace QIndependentStudios.Obex.Uwp
             return bytes;
         }
 
+        /// <summary>
+        /// Provides a way for the Obex server to send data over the connection.
+        /// </summary>
+        /// <param name="data"></param>
+        /// <returns></returns>
         public async Task WriteAsync(byte[] data)
         {
             await _socket.OutputStream.WriteAsync(data.AsBuffer());
